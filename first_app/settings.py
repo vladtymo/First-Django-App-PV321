d71dd353c7477adf35421cc1dc6287e0d7026881
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-27 20:39:38 +0200

    Allow all hosts

--- first_app/settings.py
+++ first_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-%1!2+mqf!+=jqc7hy+iku=x!ywd5@=ia-8yxx@s(c^koy_8#%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
